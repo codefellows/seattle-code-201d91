b12fa8b318dce88bddc23147473191c174d0f231
sheyna <sheynawatkins@gmail.com>
2022-10-18 23:17:28 -0700

    added class 13.

---
+++ class-13/in-class-demo/coffee-stand/js/app.js
@@ -0,0 +1,7 @@
+// a local coffee shop wants your help to build a simple application they can ues to record custsomer orders and maintain a record of what has been ordered (for any period of any amount of time).
+
+// create a very simple app that has two components
+// -- a way to create an order
+//          - form
+// -- a way to dispslay the data
+//          - lists (ul)
